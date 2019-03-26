b608c7a95dcf6f5fd9391385780022b55b04b49b
ajayraj201651006 <ajayraj003016@gmail.com>
2019-03-26 15:54:56 +0530

    create callbacks

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,18 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area =  (x,y) => (x*y);
+modules.exports = (x, y, callback) => {
+    
+    if(x<=0 || y<=0) {
+        setTimeout(() => callback(
+            new Error("Dimensions of Rectangle Should Be Grater Than 0: " + "l: " + x+ " b: " +y),
+            null
+        ), 2000);
+    } else {
+        setTimeout(() => callback(
+         null, {
+            perimeter: (x,y) => (2*(x+y)),
+            area: (x,y) => (x*y)
+         }
+        ), 2000)
+    }
+}
+
+
